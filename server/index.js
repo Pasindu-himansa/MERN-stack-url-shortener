1adf73d518a682b2709e39aa302b3784927581bc
Pasindu Subasinghe <phimansa@gmail.com>
2026-03-19 16:57:56 +0530

    README file updated

--- server/index.js
+++ server/index.js
@@ -9,13 +9,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", urlRoutes); // POST /api/shorten
-app.use("/", urlRoutes); // GET /:code redirect
+app.use("/api", urlRoutes);
+app.use("/", urlRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    serverSelectionTimeoutMS: 5000,
+  })
   .then(() => console.log("MongoDB connected"))
-  .catch((e) => console.error("Mongo error:", e.message));
+  .catch((err) => console.log("Mongo error:", err.message));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
